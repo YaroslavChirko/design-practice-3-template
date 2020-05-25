5c38de5a4bbbf0d9e36c9c1e63b4817c81cf6dbb
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2020-05-25 21:04:43 +0300

    Update balancer_test.go

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -9,14 +9,14 @@ type MySuite struct{}
 var _ = Suite(&MySuite{})
 
 func (s *MySuite) TestBalancer(c *C) {
-	traffic := []int{10,1000,100}
-	health := []bool{true,true,true}
+	traffic := [3]int{10,1000,100}
+	health := [3]bool{true,true,true}
 	i,_ := getServerMoc(traffic,health)
 	c.Assert(0,Equals,i)
 	
 	health[0]=false
 	i1,_ := getServerMoc(traffic,health)
-	c.Assert(2,Equals,i)
+	c.Assert(2,Equals,i1)
 	
 	health[1]=false
 	health[2]=false
