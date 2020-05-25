5d92472806b0eaacb04a89358bfd412c1cd4fcd8
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2020-05-25 20:59:02 +0300

    Update balancer_test.go

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,8 +1,5 @@
 package main
-import "bytes"
-import "net/http"
 import "testing"
-import "log"
 import ."gopkg.in/check.v1"
 
 func Test(t *testing.T) { TestingT(t) }
@@ -12,32 +9,18 @@ type MySuite struct{}
 var _ = Suite(&MySuite{})
 
 func (s *MySuite) TestBalancer(c *C) {
-	req, _ := http.NewRequest("POST","http://server1:8080/traffic_set",bytes.NewBuffer([]byte("10")))
-	http.DefaultClient.Do(req)
-	req, _ = http.NewRequest("POST","http://server2:8080/traffic_set",bytes.NewBuffer([]byte("1000")))
-	http.DefaultClient.Do(req)
-	req, _ = http.NewRequest("POST","http://server3:8080/traffic_set",bytes.NewBuffer([]byte("100")))
-	http.DefaultClient.Do(req)
-	/*i:=-1
-	log.Printf("Index is : %d now",i)
-	i,_=getServer(i)
-	log.Printf("Index is : %d now",i)*/
-	//c.Assert(0,Equals,i)
-	/*req, _ := http.NewRequest("GET","http://server1:8080/ill_set",nil)
-	http.DefaultClient.Do(req)
-	c.Assert(0,Equals,0)
+	traffic := []int{10,1000,100}
+	health := []bool{true,true,true}
+	i,_ := getServerMoc(traffic,health)
+	c.Assert(0,Equals,i)
 	
-	req, _ = http.NewRequest("GET","http://server1:8080/ill_set",nil)
-	http.DefaultClient.Do(req)
-	i :=1//,_=getServer(-1)
-	c.Assert(1,Equals,i)*/
-	req, _ = http.NewRequest("GET","http://server1:8080/ill_set",nil)
-	http.DefaultClient.Do(req)
-	req, _ = http.NewRequest("GET","http://server2:8080/ill_set",nil)
-	http.DefaultClient.Do(req)
-	req, _ = http.NewRequest("GET","http://server3:8080/ill_set",nil)
-	http.DefaultClient.Do(req)
+	health[0]=false
+	i1,_ := getServerMoc(traffic,health)
+	c.Assert(2,Equals,i)
 	
-	_,err:=getServer(-1)
+	health[1]=false
+	health[2]=false
+	_,err := getServerMoc(traffic,health)
 	c.Assert("No healthy servers",Equals,err.Error())
+	
 }
