1f73cf4dc66cf5f2b14667078e22742cb11aa227
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2020-06-07 21:41:45 +0300

    Add files via upload

---
+++ integration/integration_test.go
@@ -0,0 +1,51 @@
+package integration
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"flag"
+	."gopkg.in/check.v1"
+	"time"
+)
+
+const baseAddress = "http://balancer:8090"
+var target = flag.String("target", "http://localhost:8090", "request target")
+
+var client = http.Client{
+	Timeout: 3 * time.Second,
+}
+
+func Test(t *testing.T) { TestingT(t) }
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestBalancer(c *C) {
+	m :=make(map[string]int)
+	m["server1:8080"]=0
+	m["server2:8080"]=0
+	m["server3:8080"]=0
+	for i:=0;i<9;i++{
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
+		if err != nil {
+			c.Error(err)
+		}
+		c.Logf("response from %s", resp.Header.Get("lb-from"))
+		m[resp.Header.Get("lb-from")] +=1
+	}
+	
+	c.Assert(m["server1:8080"],Equals,3)
+	c.Assert(m["server2:8080"],Equals,3)
+	c.Assert(m["server3:8080"],Equals,3)
+}
+
+func (s *MySuite)BenchmarkBalancer(c *C) {
+	for i:=0;i<c.N;i++{
+	_, err2 := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
+	if err2 != nil {
+			c.Error(err2)
+		}
+	}
+	
+}
